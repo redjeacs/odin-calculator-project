7e2af722a9327262be6431552a37b5e387913384
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-20 14:38:58 +0800

    eventlisteners and storage of button information
    operator onclick stores the information in a
    variable. numBtn onclick displays the number on
    calculator screen and stores info in num1 if it's
    empty and num2 if num1 already has a number. equal
    button calls the operate function using stored
    variables as arguments.

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 let num1 = '';
 let num2 = '';
 let mathOperator = null;
+const numBtn = document.querySelectorAll('.numBtn');
+const operatorBtn = document.querySelectorAll('.operatorBtn')
+const equalBtn = document.querySelector('.btnEqual')
+let display = document.querySelector('.display');
+display.textContent = '';
 
 
 function add(a, b) {
   return a + b;
-}
+};
 
 function subtract(a, b) {
   return a - b;
-}
+};
 
 function multiply(a, b) {
   return a * b;
-}
+};
 
 function divide(a, b) {
   return a / b;
-}
+};
 
 
 
 function operate(mathOperator, a, b) {
-  a = Number(a)
+  a = Number(a);
   b = Number(b);
   switch(mathOperator) {
     case '+': 
@@ -34,7 +39,25 @@ function operate(mathOperator, a, b) {
     case '/':
       if(b === 0) {
         return null;
-      }
+      };
       return divide(a, b);
+  };
+};
+
+numBtn.forEach((button) => 
+  button.addEventListener('click', function() {
+    display.textContent = button.textContent;
+    if(num1 === '') {
+      return num1 = display.textContent;
+    }
+    return num2 = display.textContent;
   }
-}
+));
+
+operatorBtn.forEach((button) => 
+  button.addEventListener('click', () => mathOperator = button.textContent)
+);
+
+equalBtn.addEventListener('click', () => 
+  display.textContent = operate(mathOperator, num1, num2)
+);
